1c306ddaad7b6c74feac572c1021a473935f7688
poorab patel <poorabpatel202@gmail.com>
2025-09-27 15:20:46 +0530

    5th change

--- server/server.js
+++ server/server.js
@@ -13,13 +13,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());  
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 await connectDB();
