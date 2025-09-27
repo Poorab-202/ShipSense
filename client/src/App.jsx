0bf222668e110d96886ea8ae6eb76e387c0d3589
poorab patel <poorabpatel202@gmail.com>
2025-09-27 15:25:38 +0530

    6th

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Login from './components/auth/login'
+import Login from './components/auth/Login'
 import Navbar from './components/shared/Navbar'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomeScreen from './components/HomeScreen';
